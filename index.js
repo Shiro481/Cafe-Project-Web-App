6f5964ec937716b21ac748a08bbaf88e2d6b297f
Shaqlee101 <shaqleeambagan101@gmail.com>
2024-07-23 07:56:12 +0800

    adding products to orders

--- index.js
+++ index.js
@@ -1,34 +1,6 @@
 
-let products = [{
-  image: 'Images/burger.jpg',
-  category: 'Snacks',
-  name: 'Burger',
-  price: 1.89
-},
-{
-  image: 'Images/fries.jpg',
-  category: 'Snacks',
-  name: 'Fries',
-  price: 1.55
-},
-{
-  image: 'Images/iced choco.jpg',
-  category:'Drinks',
-  name: 'Iced Choco',
-  price: 1.75
-},
-{
-  image: 'Images/iced coffee.jpg',
-  category:'Drinks',
-  name: 'Iced Coffee',
-  price: 1.99
-},
-{
-  image: 'Images/siomai.jpg',
-  category:'Meals',
-  name: 'Siomai  w/ Rice',
-  price: 1.89
-}];
+import { products } from "./products.js";
+import {orders} from "./orders.js";
 
 let productHTML = '';
 products.forEach((product) => {
@@ -37,12 +9,66 @@ products.forEach((product) => {
     <div class="food-card-interior">
         <img src="${product.image}" class="food-image">
         <p class="js-food-name">${product.name}</p>
-        <p class="js-food-price">$${product.price}</p>
+        <p type="number" class="js-food-price">$${product.price}</p>
         <div class="add-button-container">
-          <button class="js-add-button">Add</button>
+          <button class="js-add-button" data-product-name="${product.name}" data-product-price="${product.price}">Add</button>
         </div>
       </div>
   `;
 });
 
 document.querySelector('.js-food-card-container').innerHTML = productHTML;
+
+document.querySelectorAll('.js-add-button').forEach((button) => {
+  button.addEventListener('click', ()=>{
+    let productName = button.dataset.productName;
+    let productPrice = button.dataset.productPrice;
+
+    let sameProduct;
+
+    orders.forEach((product) =>{
+      if (productName === product.productName){
+        sameProduct = product;
+      };
+    });
+
+    if (sameProduct){
+
+      sameProduct.quantity ++;
+    }
+    else{
+      orders.push({
+        productName: productName,
+        productPrice: Number(productPrice),
+        quantity: 1
+      });
+    };
+
+    let ordersHTML = '';
+
+    orders.forEach((item)=> {
+    
+      ordersHTML += `<div class="food-order-container">
+              <div class="food-order-container-interior">
+                <p class="js-food-name">${item.productName}</p>
+                <div class="food-quantifier">
+                  <i class="fa-solid fa-circle-minus"></i>
+                  <p class="js-counter">${item.quantity}</p>
+                  <i class="fa-solid fa-circle-plus"></i>
+                </div>
+                <p class="js-food-price">$${item.productPrice}</p>
+              </div>
+              <i class="fa-solid fa-circle-xmark"></i>
+            </div>
+    `;
+    });
+    
+    document.querySelector('.js-order-container').innerHTML = ordersHTML;
+
+
+  });
+});
+
+function addQuantity(orders){
+  orders.forEach()
+}
---
+++ products.js
@@ -0,0 +1,32 @@
+
+
+export let products = [{
+  image: 'Images/burger.jpg',
+  category: 'Snacks',
+  name: 'Burger',
+  price: 1.89
+},
+{
+  image: 'Images/fries.jpg',
+  category: 'Snacks',
+  name: 'Fries',
+  price: 1.55
+},
+{
+  image: 'Images/iced choco.jpg',
+  category:'Drinks',
+  name: 'Iced Choco',
+  price: 1.75
+},
+{
+  image: 'Images/iced coffee.jpg',
+  category:'Drinks',
+  name: 'Iced Coffee',
+  price: 1.99
+},
+{
+  image: 'Images/siomai.jpg',
+  category:'Meals',
+  name: 'Siomai  w/ Rice',
+  price: 1.89
+}];
